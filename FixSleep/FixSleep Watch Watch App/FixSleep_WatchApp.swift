d5c9d7abcae7a42e21d4a8ba9ee1152e8bcccf85
Filipe Marques <filipe.marques.it@outlook.pt>
2025-12-06 19:57:08 +0000

    Add FixSleep Xcode project with iOS and watchOS targets

---
+++ FixSleep/FixSleep Watch Watch App/FixSleep_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixSleep_WatchApp.swift
+//  FixSleep Watch Watch App
+//
+//  Created by Filipe Marques on 06/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FixSleep_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FixSleep/FixSleep/FixSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixSleepApp.swift
+//  FixSleep
+//
+//  Created by Filipe Marques on 06/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FixSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
